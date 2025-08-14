069f9cd9ec1a91cc73e6a24789b756ae1f76b6cf
Silpa Tom <silpatom@gmail.com>
2025-08-14 10:41:07 +0530

    table handling

---
+++ sampleselenium/src/test/java/seleniumProject/TableHandling.java
@@ -0,0 +1,31 @@
+package seleniumProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TableHandling extends Base{
+	
+	public void verifyTables() {
+		
+		driver.navigate().to("https://money.rediff.com/indices/nse");
+		WebElement table1= driver.findElement(By.xpath("//table[@id='dataTable']")); // to locate table
+		System.out.println(table1.getText());//to print the entire table data in console
+	//for table rows and column will be inside the tbody tag
+		WebElement row3= driver.findElement(By.xpath("	//table[@id='dataTable']/tbody/tr[3]")); // to locate row
+		System.out.println("3rd row");
+		System.out.println("/n"+row3.getText());// to print the loacte
+		//table[@id='dataTable']/tbody/tr[3]/td[2]
+		
+		WebElement col2= driver.findElement(By.xpath("	//table[@id='dataTable']/tbody/tr[3]/td[2]")); // to locate ro
+		System.out.println("2nd col");
+		System.out.println("/n"+col2.getText());// to print the loacte
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TableHandling table=new TableHandling();
+		table.browserInitializer();
+		table.verifyTables();
+	}
+
+}
--- sampleselenium/src/test/java/seleniumProject/WebElementCommands.java
+++ sampleselenium/src/test/java/seleniumProject/WebElementCommands.java
@@ -9,14 +9,14 @@ public class WebElementCommands extends Base{
 		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");	//driver is object which is user defined
 		
 		WebElement textBox = driver.findElement(By.xpath("//input[@id='single-input-field']")); //locate textBox
-		textBox.sendKeys("Hello World"); //to enter data to the textbox
+		textBox.sendKeys("Hello World"); //to enter data to the text box
 		WebElement button = driver.findElement(By.xpath("//button[@id='button-one']")); //locate the button
 		button.click(); //to click the "Show Message" button
 		WebElement textMessage = driver.findElement(By.xpath("//div[@id='message-one']")); //locate the text message printed on web page
 		System.out.println(textMessage.getText()); //to print the text of textMessage element in Console window
 		System.out.println(textMessage.getTagName()); // to print the tag name of textMessage element
 		System.out.println(textMessage.getAttribute("id")); // to print the value of 'id' attribute of textMessage element. NOT USED CURRENTLY
-		System.out.println(textMessage.getCssValue("background")); //to get the css values of webelement
+		System.out.println(textMessage.getCssValue("background")); //to get the css values of web element
 		textBox.clear(); //to clear the values in textBox
 	}
 	
